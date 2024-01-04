b39371283b471ce293e9073b30fe36ffa15291f2
anish-singh-rawat <anishsinghrawat1>
2024-01-04 15:04:06 +0530

    updated code

--- src/app/dashboard/userProfile/page.jsx
+++ src/app/dashboard/userProfile/page.jsx
@@ -36,6 +36,34 @@ const page = () => {
           <div className="id">Emplooy UID : {randomNumber2}</div>
         </div>
       </div>
+      <br />
+      <center>
+        <h2>As a React delevoper you have follow some steps : -</h2>
+      </center>
+      <br />
+      <br />
+      <div id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <img src="http://res.cloudinary.com/dfqddpjfl/image/upload/v1498777628/quality_q31k6u.png" className="aboutImage" alt="High Quality Lectures" />
+              <h2>High Quality Code</h2>
+              <p>A quality code is one that is clear, simple, well tested, bug-free, refactored, documented, and performant.</p>
+            </div>
+            <div className="col-md-4">
+              <img src="http://res.cloudinary.com/dfqddpjfl/image/upload/v1498777629/tools_yorndk.png" className="aboutImage" alt="+80H of Rich Content" />
+              <h2>Implementing app modifications.</h2>
+              <p>An implementation plan gives an outline of the specific steps needed to go through in order to end up with a well-performing app. </p>
+            </div>
+            <div className="col-md-4">
+              <img src="http://res.cloudinary.com/dfqddpjfl/image/upload/v1498777627/help_dfec8u.png" className="aboutImage" alt="Get Help Anytime!" />
+              <h2>Ability to find and correct errors effectively</h2>
+              <p>
+                Definition. Error recognition refers to the ability to recognize or detect the presence of an error; recognition may happen as the error is being made or after it has occurred.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossOrigin="anonymous"/>
+      </head>
       <body className={inter.className}> {children} </body>
     </html>
   )
